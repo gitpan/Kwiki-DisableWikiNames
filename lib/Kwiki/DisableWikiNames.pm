9335b0373c47372d0bb011a89a1e426f22264f79
Pavel V. Kaygorodov <pasha@inasan.ru>
2006-04-02 13:44:54 +0000

    Import of KPV/Kwiki-DisableWikiNames-0.02 from CPAN.
    gitpan-cpan-distribution: Kwiki-DisableWikiNames
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         KPV/Kwiki-DisableWikiNames-0.02.tar.gz
    gitpan-cpan-author:       KPV
    gitpan-cpan-maturity:     released

--- lib/Kwiki/DisableWikiNames.pm
+++ lib/Kwiki/DisableWikiNames.pm
@@ -7,7 +7,7 @@ use warnings;
 use Kwiki::Plugin -Base;
 use mixin 'Kwiki::Installer';
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 const class_id => 'disable_wiki_names';
 const class_title => "Disable WikiNames auto markup";
@@ -22,17 +22,18 @@ sub register {
     $registry->add(hook => 'page:kwiki_link', pre => 'dwn_uri_hook');
 }
 
-sub dwn_uri_hook {           ## Adopted from Kwiki::CoolURI.pm 
+sub dwn_uri_hook {   ## Adopted from Kwiki::CoolURI
     my $hook = pop;
     $hook->code(undef);
-    my ($label) = @_;  
-    $label = $self->title     
-      unless defined $label;  
-    my $class = $self->active 
-      ? '' : ' class="empty"';
-    qq($label);
+    my ($label) = @_;
+    my $page_uri = $self->uri;
+    if(!defined $label) {
+      qq($page_uri);
+    } else {
+      my $class = $self->active? '' : ' class="empty"';
+      qq(<a href="$page_uri"$class>$label</a>);
+    }
 }
-
 
 1;
 __END__
